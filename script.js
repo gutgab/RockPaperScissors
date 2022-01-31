6898002f5df0ebe52d7f165a708bc992a8c16f52
gutgab <gutgab98@gmail.com>
2022-01-31 17:53:15 -0400

    UI created

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const computerPlay =()=>{
-    let x = Math.random()*10;
-    if (x<=3.3) {
+const computerPlay = () => {
+    let x = Math.random() * 10;
+    if (x <= 3.3) {
         return "rock"
     }
-    if (x>3.3 && x<=6.6) {
+    if (x > 3.3 && x <= 6.6) {
         return "paper"
     } else {
         return "scissors"
     }
 }
-const playRound = (playerSelection,computerSelection)=>{
+const playRound = (playerSelection, computerSelection) => {
     let tie = "It's a tie both selected " + playerSelection;
-    let win = "You win! " + playerSelection + " beats " + computerSelection;
-    let lose = "You lose! " + computerSelection + " beats " + playerSelection;
+    let win = "You win this round! " + playerSelection + " beats " + computerSelection;
+    let lose = "You lose this round! " + computerSelection + " beats " + playerSelection;
 
     if (playerSelection == "rock") {
         switch (computerSelection) {
             case "paper":
-                return lose; 
+                return lose;
             case "scissors":
                 return win;
             default:
@@ -25,20 +25,20 @@ const playRound = (playerSelection,computerSelection)=>{
         }
     }
     else if (playerSelection == "paper") {
-        
+
         switch (computerSelection) {
             case "scissors":
-                return lose; 
+                return lose;
             case "rock":
                 return win;
             default:
                 return tie;
         }
     }
-    else{
+    else {
         switch (computerSelection) {
             case "rock":
-                return lose; 
+                return lose;
             case "paper":
                 return win;
             default:
@@ -46,23 +46,32 @@ const playRound = (playerSelection,computerSelection)=>{
         }
     }
 }
-
-const game = ()=>{
-    let winCount = 0;
-    for (let index = 0; index <= 4; index++) {
-        let playerSelection = prompt("Select rock, paper or scissors");
-        playerSelection.toLowerCase();
-        if (playerSelection == "rock" || playerSelection == "scissors" || playerSelection == "paper") {
-            let round = playRound(playerSelection,computerPlay());
-            if(round.slice(4,7) == "win") winCount++;
-            if(round.slice(0,1) == "I") index--;
-            console.log(round);
-        }
-        else{
-            index--;
-            alert("Select a valid option");
-        }
-    }
-    return `You win ${winCount} of 5 games`;
+const game = (playerSelection) => {
+        let round = playRound(playerSelection, computerPlay());
+        if (round.slice(4, 7) == "win") winCount++;
+        else if (round.slice(0, 1) == "I") tieCount++;
+        else loseCount++;
+        console.log(round);
+        console.log(winCount);
+        return round;
 }
-console.log(game());
+const btns = document.querySelectorAll(".btn");
+const actualRound = document.querySelector(".actualRound");
+const count = document.querySelector(".count");
+const result = document.querySelector(".result");
+let winCount = 0;
+let tieCount = 0;
+let loseCount = 0;
+
+btns.forEach(btn => btn.addEventListener("click",()=>{
+    actualRound.textContent = game(btn.id);
+    count.textContent = `Wins: ${winCount} Ties: ${tieCount} Losses: ${loseCount}`;
+    if(winCount>2 || loseCount>2){
+        if(winCount>loseCount) result.textContent = "You won";
+        else result.textContent = "You lost";
+        winCount = 0;
+        tieCount = 0;
+        loseCount = 0;
+    }
+    else result.textContent = "";
+}))
